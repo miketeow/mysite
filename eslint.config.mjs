d31941ac9a8f6a750c2a90007844b26f7390166f
miketeow <27071053+miketeow@users.noreply.github.com>
2025-11-30 17:23:16 +0800

    chore: setup more eslint rules and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,44 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import tailwind from "eslint-plugin-tailwindcss";
 import * as mdx from "eslint-plugin-mdx";
+import checkFile from "eslint-plugin-check-file";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  // 1. Formatting and Naming Conventions
+  {
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      // Enforce Arrow Functions
+      "prefer-arrow-callback": "error",
+      // Enforce template literals (`${var}`) over string concatenation (`str + str`)
+      "prefer-template": "error",
+
+      // Enforce kebab-case for filenames
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          // Ignore next.config.ts or data.test.js, ignore the middle extension like config or test
+          ignoreMiddleExtensions: true,
+        },
+      ],
+
+      // Enforce kebab-case for folder names
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "app/**/": "KEBAB_CASE",
+          "components/**/": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
 
   // 2. Tailwind CSS Rules
   // Will warn you if you write bg-red-999 (doesn't exist) or have conflicting classes
@@ -36,6 +71,8 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
+    "bun.lockb",
   ]),
 ]);
 
